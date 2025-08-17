ca0cdd6f1f93311d81efc219c6214c724e91bddb
arcfernandes04 <abby@cue-go.co.nz>
2025-08-17 14:42:27 +1200

    Adding initial routing and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home-component/home-component';
+import { NotFoundComponent } from './shared/not-found-component/not-found-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-const hello = 'd';
-const x = 'yooo';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('app');
-}
+export class App {}
