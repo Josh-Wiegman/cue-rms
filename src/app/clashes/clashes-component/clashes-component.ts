784c2f7b9384140e791e0258b0c98778148f7fca
arcfernandes04 <abby@cue-go.co.nz>
2025-08-21 20:32:47 +1200

    Implemented page routing

---
+++ src/app/clashes/clashes-component/clashes-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { UiShellComponent } from '../../shared/ui-shell/ui-shell-component';
+
+@Component({
+  selector: 'clashes-component',
+  imports: [UiShellComponent],
+  templateUrl: './clashes-component.html',
+  styleUrl: './clashes-component.scss',
+})
+export class ClashesComponent {}
---
+++ src/app/inventory/inventory-component/inventory-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { UiShellComponent } from '../../shared/ui-shell/ui-shell-component';
+
+@Component({
+  selector: 'inventory-component',
+  imports: [UiShellComponent],
+  templateUrl: './inventory-component.html',
+  styleUrl: './inventory-component.scss',
+})
+export class InventoryComponent {}
