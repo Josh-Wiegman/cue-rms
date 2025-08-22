cb756869f7e63ee3898c6a965c969df13a5b4f6c
arcfernandes04 <abby@cue-go.co.nz>
2025-08-22 13:40:18 +1200

    Product grid assembled

--- src/app/inventory/inventory-component/inventory-component.ts
+++ src/app/inventory/inventory-component/inventory-component.ts
@@ -9,5 +9,32 @@ import { ProductGridComponent } from '../../shared/product-grid/product-grid-com
   styleUrl: './inventory-component.scss',
 })
 export class InventoryComponent {
-  test: string[] = ['a', 'b', 'c', 'd'];
+  test: string[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
 }
