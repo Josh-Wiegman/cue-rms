da25a776753362842231893391f815ae3a988e55
Josh-Wiegman <josh@thecuegroup.co.nz>
2025-10-27 13:45:13 +1300

    trying to do this via org slug instead of a hard code

--- src/app/shared/supabase-service/db_functions.service.ts
+++ src/app/shared/supabase-service/db_functions.service.ts
@@ -94,7 +94,7 @@ export class dbFunctionsService {
           {
             headers: {
               'x-query-type': 'navigation-items',
-              'x-org-id': orgId,
+              'x-org-slug': 'x-org-slug',
             },
           },
         );
