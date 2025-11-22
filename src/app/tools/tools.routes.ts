4d6512cb4125f996fefc95ede86e4afc6d83db01
Josh-Wiegman <josh@thecuegroup.co.nz>
2025-11-22 15:13:36 +1300

    Minor bug fix?

--- src/app/tools/tools.routes.ts
+++ src/app/tools/tools.routes.ts
@@ -14,23 +14,23 @@ export const ToolsRoutes: Routes = [
       {
         path: 'stage-timer',
         loadComponent: () =>
-          import('./stage-timer-dashboard/stage-timer-dashboard.component').then(
-            (m) => m.StageTimerDashboardComponent,
-          ),
+          import(
+            './stage-timer-dashboard/stage-timer-dashboard.component'
+          ).then((m) => m.StageTimerDashboardComponent),
       },
       {
         path: 'stage-timer/presenter',
         loadComponent: () =>
-          import('./stage-timer-presenter-access/stage-timer-presenter-access.component').then(
-            (m) => m.StageTimerPresenterAccessComponent,
-          ),
+          import(
+            './stage-timer-presenter-access/stage-timer-presenter-access.component'
+          ).then((m) => m.StageTimerPresenterAccessComponent),
       },
       {
         path: 'stage-timer/presenter/:code',
         loadComponent: () =>
-          import('./stage-timer-presenter/stage-timer-presenter.component').then(
-            (m) => m.StageTimerPresenterComponent,
-          ),
+          import(
+            './stage-timer-presenter/stage-timer-presenter.component'
+          ).then((m) => m.StageTimerPresenterComponent),
       },
     ],
   },
